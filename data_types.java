6c6133298e5e4a1e269cc6ea8a5d90b64a023601
Dhanyasri-Katta <2420030023@klh.edu.in>
2025-08-02 09:46:39 +0530

    added new file to practical repository

---
+++ data_types.java
@@ -0,0 +1,26 @@
+package My_project;
+
+public class data_types {
+	public static void main(String args[]) {
+		String name="Dhanya Sri";
+		String dept="CSE";
+		 // long roll_no;
+	    long roll_no=2420030023L;
+		//long phn_no;
+		long phn_no=3674924722L;
+		String add="HYD";
+		/*int a=2;
+		char b='a';
+		double c=4.5;
+		float d=7.88f;
+		String s="hello this is klh";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(s);*/
+	  //System.out.println(s+","+a+","+b+","+c+","+d);
+	//  System.out.printf("integer %d%ncharacter  %c/ndouble %f/nstring %s",a,b,c,s);
+		System.out.printf("name: %s%ndept: %s%nroll_no: %d%nphn_no: %d%nadd: %s",name,dept,roll_no,phn_no,add);
+	}
+}
